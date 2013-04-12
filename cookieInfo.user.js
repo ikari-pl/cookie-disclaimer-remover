5d4cae80512a2f3018aba467b2900ff3764d3a24
Cezar "ikari" Pokorski <git@ikari.pl>
2013-04-12 22:28:58 +0200

    harder removing ("cookie" anywhere in ID), additional check in a timeout (works for onet.pl)

--- cookieInfo.user.js
+++ cookieInfo.user.js
@@ -2,31 +2,39 @@
 // @name        Cookie disclaimer remover
 // @description God how we in Europe hate the idiotic law that enforces every site to display a huge disclaimer about cookies being used!
 // @include     http*://*
-// @version     0.3
+// @version     0.4
 // @updateURL   https://raw.github.com/ikari-pl/cookie-disclaimer-remover/master/cookieInfo.user.js
 // @downloadURL https://raw.github.com/ikari-pl/cookie-disclaimer-remover/master/cookieInfo.user.js
 // @copyright   2013+, Cezar "ikari" Pokorski
 // ==/UserScript==
 
 function cookieDisclaimerRemove() {    
-    var i, e, target = document.querySelectorAll('*[id^="cookie"], *[class^="cookie"]');
-    console.log("Cookie disclaimer remover started! :D");
+    var i, e, target = document.querySelectorAll(
+        '*[id*="cookie"], *[class^="cookie"]'
+        );
+    log("doing a DOM checkup");
     if (target.length > 0) {
         for (i = 0; i < target.length; i++) {
-            if (!(target[i])) return;
+            if (!(target[i])) return; // just in case something vanished
             e = target[i];
             e.parentNode.removeChild(e);
-            console.log("Cookie disclaimer found and removed:");
+            log("found and removed:");
             console.debug(e);
         }
+    } else {
+        log("nothing to remove")
     }
 }
 
+function log(message) { console.log("[cookie-disclaimer-remover] "+message); }
+console.log("Cookie disclaimer remover started! :D");
 if (document.readyState === "complete" || document.readyState === "interactive") {
     cookieDisclaimerRemove();
+    setTimeout(cookieDisclaimerRemove, 600); // because onet.pl
 } else {
     if (document.addEventListener) {
         document.addEventListener("DOMContentLoaded", cookieDisclaimerRemove, false);
+
     } else {
         console.log("If you can see this, I don't like your browser. "+
             "I am a lazy developer that wanted the first version to support "+
